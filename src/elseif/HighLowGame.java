f6928167f1d2b95b7b808ab9f4cbee7de5a1109a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-02-03 10:57:44 -0800

    Robot color chooser did not work but i suceed in high low

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,28 +5,43 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(101);
 		// 2. Print out the random variable above
-		
+		System.out.println(random);
 		// 11. do the following 10 times
+		for (int i = 0; i < 10; i++) {
+			
 		
 			// 1. ask the user for a guess using a pop-up window, and save their response 
-
-			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
+		String guess =JOptionPane.showInputDialog("Guess a number");
+		// 4. convert the users’ answer to an int (Integer.parseInt(string))
+		int number = Integer.parseInt(guess);
+		
 			// 5. if the guess is correct
-				// 6. win
+				if(number==random) {JOptionPane.showMessageDialog(null,"YOU LOST !!!!! HAHAHAHA Im kidding you sadly won wooooo im so proud of you (that was suppused to be sarcastic)");}
+		
+		// 6. win
 			// 7. if the guess is high
+				else if(number>random) {JOptionPane.showMessageDialog(null, "Too high guess again loser ");}
+				
 				// 8. tell them it's too high
-			// 9. if the guess is low
+			
+				// 9. if the guess is low
+				else if (number<random) {JOptionPane.showMessageDialog(null, "too low try again loser");}
+				
 				// 10. tell them it's too low
-
+		}
 		// 12. tell them they lose
+		JOptionPane.showMessageDialog(null, "HAHAHAHAHAHAHAHAHAHAAHAHHHHAAHhahAHHAAHAHAHAHAHAHAHAHHAHAAHAHAHHAAHHAHAHAHAHAHAHAAHHAAHAHHAAHHA YOU LOST LOSER AHAHAHHAAHAHAHAHAHAHHAAHAHHAHAAHHAHAHHAAHHAHAHAAHAHHAHAAHAHAHHAHAHAAHAHAAHAHAHHAAHAHAHAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHAHAHHAAHHAHAHAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHAAHHAHAHAAHHAHAAHAHAHAHAHAHAHAHAHHAHAAHHAAHAHAHAHHAHAAHAHHAHAAHAHAHAHAHAHAHAH");
 	}
+	
+	
 
 }
 
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -10,10 +10,7 @@ import org.jointheleague.graphical.robot.Robot;
 
 import java.awt.Color;
 
-public class RobotColorChooser {
-	public static void main(String[]
-			
-args) {
+public class RobotColorChooser {public static void main(String[] args) {
 	
 		
 		
@@ -27,10 +24,11 @@ if(color.equals("red"))
         //6. put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		 //2. set the pen width to 10
-		r2d2.setpenwidth()
+		
 		//1. make the robot draw a shape (this will take more than one line of code)
 		Robot r2d2=new Robot();
-		r2d2.move(70);
+r2d2.setPenWidth(2);
+r2d2.move(70);
 		r2d2.turn(120);
 		r2d2.move(70);
 		r2d2.turn(120);
