997455e3e4ecee91b56bcc1c553cbf951175508c
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-24 10:53:55 -0700

    color chooser

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -10,34 +10,44 @@ import org.jointheleague.graphical.robot.Robot;
 
 import java.awt.Color;
 
-public class RobotColorChooser {public static void main(String[] args) {
-	
-		
-		
-	  
-		//3. ask the user what color they would like the robot to draw
-		String color =JOptionPane.showInputDialog("Enter a color");
-		//4. use an if/else statement to set the pen color that the user requested
-if(color.equals("red"))
-        //5. if the user doesn’t enter anything, choose a random color
+public class RobotColorChooser {
+	public static void main(String[] args) {
 
-        //6. put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		 //2. set the pen width to 10
-		
-		//1. make the robot draw a shape (this will take more than one line of code)
-		Robot r2d2=new Robot();
-r2d2.setPenWidth(2);
-r2d2.move(70);
-		r2d2.turn(120);
-		r2d2.move(70);
-		r2d2.turn(120);
-		r2d2.move(70);
-		r2d2.turn(120);
-		
-	   
-	
-	
-	
+		// 3. ask the user what color they would like the robot to draw
+
+		// 4. use an if/else statement to set the pen color that the user requested
+
+		// 5. if the user doesn’t enter anything, choose a random color
+
+		// 6. put a loop around your code so that you keep asking the user for more
+		// colors & drawing them
+
+		// 2. set the pen width to 10
+
+		// 1. make the robot draw a shape (this will take more than one line of code)
+		for (int i = 0; i < args.length; i++) {
+			String color = JOptionPane.showInputDialog("Choose a color(Red,Cyan,Black or random)");
+			Robot r2d2 = new Robot();
+			if (color.equals("red")) {
+				r2d2.setPenColor(Color.RED);
+			} else if (color.equals("cyan")) {
+				r2d2.setPenColor(Color.cyan);
+			} else if (color.equals("black")) {
+				r2d2.setPenColor(Color.black);
+			} else {
+				r2d2.setRandomPenColor();
+			}
+
+			r2d2.setPenWidth(10);
+			r2d2.penDown();
+			r2d2.move(80);
+			r2d2.turn(90);
+			r2d2.move(80);
+			r2d2.turn(90);
+			r2d2.move(80);
+			r2d2.turn(90);
+			r2d2.move(80);
+			r2d2.turn(90);
+		}
 	}
 }
